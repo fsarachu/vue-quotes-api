b0176a8f37b181177a7d45d76d9696f118e265c1
fsarachu <fsarachu@gmail.com>
2017-04-11 04:19:01 -0300

    Implement signin method in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
 
 class UserController extends Controller
 {
@@ -24,5 +26,26 @@ class UserController extends Controller
         $user->save();
 
         return response()->json(['message' => 'User successfully created'], 201);
+    }
+
+    public function signin(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Couldn\'t sign in'], 500);
+        }
+
+        return response()->json(['token' => $token], 200);
     }
 }
